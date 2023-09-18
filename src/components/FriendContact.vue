2b4223374092efd863e92eaef41d26c650fe4169
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2023-09-18 13:42:23 +0200

    'Changing' props by copying passed props from parent to child

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,6 +1,7 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone:</strong>{{ phoneNumber }}</li>
@@ -14,17 +15,26 @@ export default {
   props: [
     'name',
     'phoneNumber',
-    'emailAddress'
+    'emailAddress',
+    'isFavorite'
   ],
   data() {
     return {
       detailsAreVisible: false,
-      friend: { id: 'manuel' , name: 'Manuel Lorenz', phone: '12344', email: 'manu@local.com'}
+      friend: { id: 'manuel' , name: 'Manuel Lorenz', phone: '12344', email: 'manu@local.com'},
+      friendIsFavorite: this.isFavorite
     }
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible
+    },
+    toggleFavorite() {
+      if(this.friendIsFavorite === '1') {
+        this.friendIsFavorite = '0'
+      } else {
+        this.friendIsFavorite = '1'
+      }
     }
   }
 }
