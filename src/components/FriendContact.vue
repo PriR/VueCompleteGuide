37f55604a407cbd2f773408992240a611bcf2fb6
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2023-09-18 11:31:30 +0200

    Initi

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: { id: 'manuel' , name: 'Manuel Lorenz', phone: '12344', email: 'manu@local.com'}
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
+
